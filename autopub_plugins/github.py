fddf47dd28323c9c90be6ed8ba0c92c620cd83e6
Patrick Arminio <patrick.arminio@gmail.com>
2023-12-08 15:16:39 +0100

    New

--- autopub_plugins/github.py
+++ autopub_plugins/github.py
@@ -7,7 +7,7 @@ from github import Github
 from autopub.exceptions import AutopubException
 from autopub.types import ReleaseInfo
 from github import Auth
-
+import textwrap
 from autopub.plugins import AutopubPlugin
 
 
@@ -25,14 +25,49 @@ class GithubPlugin(AutopubPlugin):
         return json.loads(pathlib.Path(path).read_text())
 
     def on_release_notes_invalid(self, exception: AutopubException):
-        self._send_comment("Release notes are invalid", exception.message)
+        text = textwrap.dedent(
+            f"""
+            Your release notes are invalid. Please fix them and try again.
+
+            The following error was raised:
+
+            ```
+            {exception}
+            ```
+            """
+        ).strip()
+
+        self._send_comment(text)
 
     def on_release_notes_valid(self, release_info: ReleaseInfo):
-        # TODO: print release info
-        self._send_comment("Release notes are valid", "Good job!")
+        text = textwrap.dedent(
+            f"""
+            Your release notes look good!
 
-    def _send_comment(self, title: str, body: str):
+            ## Release Info
+
+            - **Version:** {release_info.version}
+            - **Release type:** {release_info.release_type}
+
+            ## Release Notes
+            """
+        ).strip()
+
+        text += "\n\n" + release_info.release_notes
+
+        self._send_comment(text)
+
+    def _send_comment(self, body: str):
         repo = self.github.get_repo(self.event["repository"]["full_name"])
         pr = repo.get_pull(self.event["pull_request"]["number"])
 
-        pr.create_issue_comment(f"## {title}\n\n{body}")
+        comment_signature = "<!-- autopub-release-check ✨ -->"
+
+        message = f"{body}\n\n{comment_signature}"
+
+        for comment in pr.get_issue_comments():
+            if comment.body.endswith(comment_signature):
+                comment.edit(message)
+                return
+
+        pr.create_issue_comment(message)
