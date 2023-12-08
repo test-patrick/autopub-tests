ca9db1197b5a48211a020add18c8760b28e57594
Patrick Arminio <patrick.arminio@gmail.com>
2023-12-08 14:08:31 +0100

    Fix sending comment

--- autopub_plugins/github.py
+++ autopub_plugins/github.py
@@ -5,6 +5,7 @@ import json
 import pathlib
 from github import Github
 from autopub.exceptions import AutopubException
+from autopub.types import ReleaseInfo
 from github import Auth
 
 from autopub.plugins import AutopubPlugin
@@ -24,4 +25,14 @@ class GithubPlugin(AutopubPlugin):
         return json.loads(pathlib.Path(path).read_text())
 
     def on_release_notes_invalid(self, exception: AutopubException):
-        print(self.event)
+        self._send_comment("Release notes are invalid", exception.message)
+
+    def on_release_notes_valid(self, release_info: ReleaseInfo):
+        # TODO: print release info
+        self._send_comment("Release notes are valid", "Good job!")
+
+    def _send_comment(self, title: str, body: str):
+        repo = self.github.get_repo(self.event["repository"]["full_name"])
+        pr = repo.get_pull(self.event["pull_request"]["number"])
+
+        pr.create_issue_comment(f"## {title}\n\n{body}")
