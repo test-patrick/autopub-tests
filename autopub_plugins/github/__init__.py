7f03106e1128c476bbbaf14a87f7e9c87dea06c6
Patrick Arminio <patrick.arminio@gmail.com>
2023-12-10 20:38:14 +0100

    Split

---
+++ autopub_plugins/github/__init__.py
@@ -0,0 +1,8 @@
+from .github_pr_comment import GithubPRCommentPlugin as PRCommentPlugin
+from .github_release import GithubReleasePlugin as ReleasePlugin
+
+
+__all__ = [
+    "PRCommentPlugin",
+    "ReleasePlugin",
+]
